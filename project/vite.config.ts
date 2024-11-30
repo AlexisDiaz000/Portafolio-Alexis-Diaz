863dfcb41e1297ab13f46f582cad218825fa9956
alexis diaz <axel.608117@gmail.com>
2024-11-30 16:59:10 -0500

    Cambiar a rutas relativas en vite.config.ts

--- project/vite.config.ts
+++ project/vite.config.ts
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/Portafolio-Alexis-Diaz/', // Corregido para coincidir con el nombre exacto del repositorio
+  base: './', // Cambiado para usar rutas relativas
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
